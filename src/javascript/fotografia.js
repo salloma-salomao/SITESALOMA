b87111f6c3fbbdd13c93a69ae8d44375e38166ff
Andre Leite <andreleitep@gmail.com>
2026-05-03 13:59:01 -0300

    Criação do Acervo Fotográfico

---
+++ src/javascript/fotografia.js
@@ -0,0 +1,141 @@
+const galleries = {
+  1: [
+    "src/images/Acervo/A/01.jpg",
+    "src/images/Acervo/A/02.jpg",
+    "src/images/Acervo/A/03.jpg",
+    "src/images/Acervo/A/04.jpg",
+    "src/images/Acervo/A/05.jpg",
+    "src/images/Acervo/A/06.jpg",
+    "src/images/Acervo/A/07.jpg",
+    "src/images/Acervo/A/08.jpg",
+    "src/images/Acervo/A/09.jpg",
+    "src/images/Acervo/A/10.jpg",
+    "src/images/Acervo/A/11.jpg",
+    "src/images/Acervo/A/12.jpg",
+    "src/images/Acervo/A/13.jpg",
+    "src/images/Acervo/A/14.jpg",
+    "src/images/Acervo/A/15.jpg",
+    "src/images/Acervo/A/16.jpg",
+    "src/images/Acervo/A/17.jpg",
+    "src/images/Acervo/A/18.jpg",
+    "src/images/Acervo/A/19.jpg",
+    "src/images/Acervo/A/20.jpg",
+    "src/images/Acervo/A/21.jpg",
+    "src/images/Acervo/A/22.jpg",
+    "src/images/Acervo/A/23.jpg",
+    "src/images/Acervo/A/24.jpg",
+    "src/images/Acervo/A/25.jpg",
+    "src/images/Acervo/A/26.jpg",
+    "src/images/Acervo/A/27.jpg",
+    "src/images/Acervo/A/28.jpg",
+    "src/images/Acervo/A/29.jpg"
+  ],
+  2: [
+    "src/images/Acervo/B/01.jpg",
+    "src/images/Acervo/B/02.jpg",
+    "src/images/Acervo/B/03.jpg",
+    "src/images/Acervo/B/04.jpg",
+    "src/images/Acervo/B/05.jpg",
+    "src/images/Acervo/B/06.jpg",
+    "src/images/Acervo/B/07.jpg",
+    "src/images/Acervo/B/08.jpg",
+    "src/images/Acervo/B/09.jpg",
+    "src/images/Acervo/B/10.jpg",
+    "src/images/Acervo/B/11.jpg",
+    "src/images/Acervo/B/12.jpg",
+    "src/images/Acervo/B/13.jpg",
+    "src/images/Acervo/B/14.jpg",
+    "src/images/Acervo/B/15.jpg",
+    "src/images/Acervo/B/16.jpg",
+    "src/images/Acervo/B/17.jpg",
+    "src/images/Acervo/B/18.jpg",
+    "src/images/Acervo/B/19.jpg",
+    "src/images/Acervo/B/20.jpg",
+    "src/images/Acervo/B/21.jpg",
+    "src/images/Acervo/B/22.jpg",
+    "src/images/Acervo/B/23.jpg",
+    "src/images/Acervo/B/24.jpg",
+    "src/images/Acervo/B/25.jpg",
+    "src/images/Acervo/B/26.jpg",
+    "src/images/Acervo/B/27.jpg",
+    "src/images/Acervo/B/28.jpg",
+    "src/images/Acervo/B/29.jpg",
+    "src/images/Acervo/B/30.jpg",
+    "src/images/Acervo/B/31.jpg",
+    "src/images/Acervo/B/32.jpg",
+    "src/images/Acervo/B/33.jpg",
+    "src/images/Acervo/B/34.jpg",
+    "src/images/Acervo/B/35.jpg",
+    "src/images/Acervo/B/36.jpg",
+    "src/images/Acervo/B/37.jpg",
+    "src/images/Acervo/B/38.jpg"
+  ],
+  3: [
+    "src/images/Acervo/C/01.jpg",
+    "src/images/Acervo/C/02.jpg",
+    "src/images/Acervo/C/03.jpg",
+    "src/images/Acervo/C/04.jpg",
+    "src/images/Acervo/C/05.jpg",
+    "src/images/Acervo/C/06.jpg",
+    "src/images/Acervo/C/07.jpg",
+    "src/images/Acervo/C/08.jpg",
+  ]
+};
+
+document.querySelectorAll(".gallery").forEach(gallery => {
+  const id = gallery.dataset.gallery;
+  const images = galleries[id];
+  let index = 0;
+
+  const imgElement = gallery.querySelector("img");
+
+  function loadImage(i) {
+    const img = new Image();
+    img.src = images[i];
+
+    img.onload = () => {
+      imgElement.src = img.src;
+    };
+  }
+
+  // carregar primeira imagem
+  loadImage(index);
+
+  gallery.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    loadImage(index);
+  });
+
+  gallery.querySelector(".prev").addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    loadImage(index);
+  });
+});
+
+// Função para scroll suave para contato
+function scrollToContact() {
+    const contactSection = document.getElementById('contato');
+    if (contactSection) {
+        contactSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+}
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = lightbox.querySelector("img");
+
+// abrir ao clicar na imagem da galeria
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.classList.remove("hidden");
+  });
+});
+
+// fechar ao clicar em qualquer lugar
+lightbox.addEventListener("click", () => {
+  lightbox.classList.add("hidden");
+  lightboxImg.src = "";
+});
